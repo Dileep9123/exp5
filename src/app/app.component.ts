661b308fde646f48482870e557d83005d1a64073
Dileep9123 <gunduboinadileep@gmail.com>
2024-11-05 00:23:21 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exp5';
+
+  list:any[] = [];
+
+  addTask(item : string){
+    this.list.push({id:this.list.length, name:item});
+    console.warn(this.list);
+  }
+
+  removeTask(id : number){
+    this.list = this.list.filter(item => item.id !== id);
+  }
 }
